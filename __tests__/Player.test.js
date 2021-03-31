7f72794e62089969a914bf6334a01a1afe1b0e4d
Ryan Helm <rjhelm@github.io>
2021-03-31 14:43:39 -0600

    function for reducing player health

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -39,3 +39,25 @@ test("gets player's health value", () => {
 
     expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
 });
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth- 5);
+
+    player.reduceHealth(999999);
+    expect(player.health).toBe(0);
+});
