3fcd7540cb07c8e2bece59783fae2fdbebec5eb2
Ryan Helm <rjhelm@github.io>
2021-03-31 15:33:32 -0600

    added test for producing enemy

---
+++ __tests__/Enemy.test.js
@@ -0,0 +1,16 @@
+const { TestScheduler } = require('@jest/core');
+const Enemy = require('../lib/Enemy');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+TestScheduler('creates an enemy object', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+});
